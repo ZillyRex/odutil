5b3246309037e1243c1d511b88b5465873403daf
ZillyRex <zillyrain@gmail.com>
2020-05-07 18:53:44 +0800

    change parser to analysis and fix it. add visual.

--- analysis.py
+++ analysis.py
@@ -23,6 +23,7 @@ def parse_anno(path_anno):
     """
     res = {}
     tree = ET.ElementTree(file=path_anno)
+
     # Parse annotation name
     res['annoname'] = os.path.basename(path_anno)
 
@@ -33,7 +34,7 @@ def parse_anno(path_anno):
     size = tree.find('size')
     dict_size = {}
     for item in size:
-        dict_size[item.tag] = item.text
+        dict_size[item.tag] = int(float(item.text))
     res['size'] = dict_size
 
     # Parse object
@@ -44,7 +45,7 @@ def parse_anno(path_anno):
         dict_obj['name'] = obj.find('name').text
         bbox = obj.find('bndbox')
         for item in bbox:
-            dict_obj[item.tag] = item.text
+            dict_obj[item.tag] = int(float(item.text))
         res['object'].append(dict_obj)
     return res
 
@@ -70,4 +71,30 @@ def parse_annos(path_annos):
     path_annos = [os.path.join(path_annos, i) for i in os.listdir(path_annos)]
     pool = Pool(cpu_count())
     res = pool.map(parse_anno, path_annos)
+    pool.close()
+    pool.join()
     return res
+
+
+def check_match(path_1, path_2):
+    """
+    Check if the file names in the folders match each other.
+
+    Param:
+        path_1: Path of a folder.
+        path_2: Path of another folder.
+
+    Return:
+        True if match else False.
+    """
+    name_1 = os.listdir(path_1)
+    name_2 = os.listdir(path_2)
+    if len(name_1) != len(name_2):
+        return False
+    set_name = set()
+    for name in name_1:
+        set_name.add(os.path.splitext(name)[0])
+    for name in name_2:
+        if os.path.splitext(name)[0] not in set_name:
+            return False
+    return True
